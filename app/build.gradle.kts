76208245a77cec790003e1ff7ea743e14d30c518
Teguh Rahmat Syahputra <teguhrahmat911@gmail.com>
2024-11-19 23:04:53 +0700

    Login & register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.annotation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
